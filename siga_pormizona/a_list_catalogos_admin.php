e802ea592d3add0a888cf53a60bd3d7bbb9549d8
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-04-23 11:10:37 -0500

    VARIOS

--- siga_pormizona/a_list_catalogos_admin.php
+++ siga_pormizona/a_list_catalogos_admin.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CATALOGOS PMZ</title>
+<title>LISTA CATALOGOS PMZ</title>
 <style type="text/css">
 .TITULO_NARANJA {
 	color: #FC0;
