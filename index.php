da5d50ce398dd54335fc8df2bd53e45c32bb2b88
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-05-19 14:39:27 -0500

    index1

--- index.php
+++ index.php
@@ -2,18 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta charset="UTF-8">
-    
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta name="viewport" content="width=600">
-  -->
-
 <meta name="robots" content="index, follow" />
 <meta name="keywords" content="www.pormizona.com.pe: pormizona.com.pe:: pormizona :: avisos :: eventos : eventos en arequipa :porongoche : catalogo : pormizona catalogo : pormizona eventos :: por mizona .com .pe :: publicidad por mi zona :: AQP :: busco :: Por Mi Zona, eventos, catálogo, oubkictario,  Arequipa, avisos por mi zona, busco eventos., que hago hoy dia" />
     <meta name="description" content="pormizona.com.pe:: pormizona :: por mi zona :: por mi zona . com.pe :: pormizona :: PORMIZONA.COM.PE :: AQP :: catálogo publicitario por mi zona :: publicidad por mi zona :: eventos por mi zona :: syscomputer :: jpawaj:: mujer bonita :: Boutique :: mujer bonita boutique" />
 
 <meta name="googlebot" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1">
     
-<title>PorMiZona.com.pe</title> 
+<title>PorMiZona.com.pe - EVENTOS</title> 
 
 <style type="text/css">
 .blanco {
@@ -35,7 +30,7 @@
 </head>
 
 <body>
-<table width="908" height="4092" border="1" align="center" cellpadding="0" cellspacing="0">
+<table width="908" height="2973" border="1" align="center" cellpadding="0" cellspacing="0">
   <tr>
     <td width="900" height="119" align="center" valign="middle"><img src="imagenes_1/logo_pmz_telf_rgb.jpg" width="862" height="158" /></td>
   </tr>
@@ -73,34 +68,7 @@
     <td height="44" bgcolor="#48887A" class="TITULOBLANCO">INDICACIONES - CATÁLOGO PUBLICITARIO</td>
   </tr>
   <tr>
-    <td height="1312" align="center" valign="top"><img src="imagenes_1/texto_comunica_pormizona.jpg" width="900" height="1285" /></td>
-  </tr>
-  <tr>
-    <td height="41" bgcolor="#48887A" class="TITULOBLANCO">PRECIOS</td>
-  </tr>
-  <tr>
-    <td height="365"><table width="907" border="1" cellspacing="1" cellpadding="1">
-      <tr>
-        <td width="432" align="center"><img src="imagenes_1/portada_manos.jpg" width="256" height="244" /></td>
-        <td width="462" align="center" bgcolor="#FFFFFF"><span class="text_tabla_zonas"><img src="imagenes_1/solo_portada.jpg" width="251" height="355" /></span></td>
-      </tr>
-    </table></td>
-  </tr>
-  <tr>
-    <td height="365"><table width="907" border="1" cellspacing="1" cellpadding="1">
-      <tr>
-        <td width="435" align="center"><img src="imagenes_1/contra_portada_manos.jpg" width="265" height="244" /></td>
-        <td width="459" align="center" bgcolor="#FFFFFF"><img src="imagenes_1/solo_contraportada.jpg" width="251" height="355" /></td>
-      </tr>
-    </table></td>
-  </tr>
-  <tr>
-    <td height="365"><table width="907" border="1" cellspacing="1" cellpadding="1">
-      <tr>
-        <td width="394" align="center"><img src="imagenes_1/central_manos.jpg" width="300" height="244" /></td>
-        <td width="500"><img src="imagenes_1/solo_central.jpg" width="500" height="355" /></td>
-      </tr>
-    </table></td>
+    <td height="1295" align="center" valign="top"><img src="imagenes_1/texto_comunica_pormizona.jpg" width="900" height="1285" /></td>
   </tr>
   <tr>
     <td height="24">&nbsp;</td>
