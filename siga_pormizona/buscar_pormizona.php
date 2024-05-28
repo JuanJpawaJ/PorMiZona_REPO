a7a1e00d1185841cd8792f5767809e1422e08416
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-05-28 14:29:15 -0500

    ffff

--- siga_pormizona/buscar_pormizona.php
+++ siga_pormizona/buscar_pormizona.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>LP - CLIENTES</title>
+<title>BUSCO Por Mi Zona</title>
 <style type="text/css">
 .TITULO_NARANJA {
 	color: #FC0;
