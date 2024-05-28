f4c42178ba3d5fa51512ee6b3fe203dcbaf6538b
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-05-28 10:43:35 -0500

    neuva

--- siga_pormizona/formingre1.php
+++ siga_pormizona/formingre1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario1</title>
+    <title>PMZ Ingrese su Negocio</title>
     <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet" href="fuentes.css">
     <link rel="stylesheet" href="estilos.css">
