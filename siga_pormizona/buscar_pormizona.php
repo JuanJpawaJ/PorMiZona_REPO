f98fd81c1d0a8852e21dadc6687a8e853bb7adc1
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-05-28 14:18:20 -0500

    AVER 3

--- siga_pormizona/buscar_pormizona.php
+++ siga_pormizona/buscar_pormizona.php
@@ -1,35 +1,67 @@
-<!DOCTYPE html>
-<html lang="es">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
-    <link rel="stylesheet" href="normalize.css">
-    <link rel="stylesheet" href="fuentes.css">
-    <link rel="stylesheet" href="estilos.css">
-    <link rel="stylesheet" href="estilos-footer.css">
-    <link rel="icon" href="imagenes/dencuentro.ico" />
-    <style type="text/css">
-    a:link {
-	text-decoration: none;
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LP - CLIENTES</title>
+<style type="text/css">
+.TITULO_NARANJA {
+	color: #FC0;
+	font-weight: bold;
 }
-a:visited {
-	text-decoration: none;
+.diez {	font-size: 10px;
 }
-a:hover {
-	text-decoration: none;
+
+.once {	font-size: 11px;
 }
-a:active {
-	text-decoration: none;
+.texto_tablas11 {	font-size: 11px;
 }
-    </style>
+.tabla10 {
+	font-size: 10px;
+	font-family: Tahoma, Geneva, sans-serif;
+}
+.tabla20 {
+	font-size: 20px;
+	font-family: Tahoma, Geneva, sans-serif;
+}
+
+.TITULO {
+	font-size: 12px;
+	color: #000;
+}
+.tit_menu_sup {
+	color: #000;
+}
+.PRECIO1 {
+	color: #009;
+	font-weight: bold;
+		font-size: 18px;
+}
+.PRECIO2 {
+	color: #999;
+	/*font-weight: bold; */
+		font-size: 12px;
+}
+
+.viewtexto {
+	font-family: "Arial";
+	font-size: 20px;
+	color: #009;
+}
+.tachado {
+    text-decoration:line-through;
+    color: red;
+	font-size: 12px;
+}
+
+
+</style>
 </head>
-<body>
-<?    
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<?php
 include("connec_sql_new.php");
 mysqli_set_charset($connec,'utf8'); 
 date_default_timezone_set("America/Lima");
 setlocale(LC_ALL, "sp");
+
 
 $longitud = $_POST['longitude'];
 $latitud = $_POST['latitude'];
@@ -47,44 +79,69 @@ if ($latitud<0) {
      $latitud = substr($latitud,0,5);
 }
 
-//echo ("Longitud: ".$longitud."<br>");
-//echo ("Latitud : ".$latitud."<br>");
+
+
+
+
+$bxproducto=$_GET['bxproducto'];
+// ********  ADICIONA, MODIFICA, ELIMINA REGISTROS 
+$viewmodi=$_GET['viewmodi'];
+$xgl=$_GET['xgl'];
+
 
 $result=mysqli_query($connec,"select * from asociado_51 where (latitud_aso like '%$latitud%' AND longitud_aso like '%$longitud%') order by categoria_aso");
 
 $total=mysqli_num_rows($result);
 //echo "Total: ".$total."<br>";
 
+
 ?>
 
+  <table width="778" border="1" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td align="center" class="tit_menu_sup">POR MI ZONA</td>
+    </tr>
+  <tr class="tit_menu_sup">
+    <td width="774" height="86" align="center" bgcolor="#FFFFCC"><table width="735" border="1" cellspacing="1" cellpadding="0">
+      <tr>
 
-    <nav>
+        <td width="92" height="75" align="center" <? if ($xgl=="S") {?> bgcolor="#FFF00" <? } ?> class="tabla10"><a href="a_lisimagenes.php?xgl=S"><img src="../imagenes/ico_p_informatica.png" width="72" height="58"></a></td>
+        <td width="92" align="center" <? if ($xgl=="M") {?> bgcolor="#FFF00" <? } ?> class="tabla10"><a href="a_lisimagenes.php?xgl=M"><img src="../imagenes/ico_p_boutique.png" width="72" height="58"></a></td>
+        <td width="92" align="center" <? if ($xgl=="R") {?> bgcolor="#FFF00" <? } ?> class="tabla10"><a href="a_lisimagenes.php?xgl=R"><img src="../imagenes/ico_p_regalos.png" width="72" height="58"></a></td>
+        <td width="92" align="center" <? if ($xgl=="P") {?> bgcolor="#FFF00" <? } ?> class="tabla10"><a href="a_lisimagenes.php?xgl=P"><img src="../imagenes/ico_p_perfumeria.png" width="72" height="58"></a></td>
+        <td width="350" align="center">
+          <form id="form0" name="form0" method="get" action="a_lisimagenes.php">
+            <table width="334" border="1" align="center" cellpadding="0" cellspacing="0" class="tablaingrenuevo">
+              <tr>
+                <td width="203" height="28" bgcolor="#FFCC66"> <span class="TITULO">Dato a buscar Producto:</span>                  <input name="bxproducto" type="text" id="bxproducto" size="25" maxlength="60" /></td>
+                
+                <td width="125" bgcolor="#FFCC66"><input name="Submit3" type="submit" class="Estilo38" value="-&gt; Buscar &lt;-" /></td>
+                
+                </tr>
+              </table>
+            </form>
+          
+          </td>
+        </tr>
+    </table></td>
+    </tr>
+  <tr class="tit_menu_sup">
+    <td height="262" valign="top" bgcolor="#FFFFCC">
+      <!-- INICIO DE MUESTRA ITEMS -->
+      <table width="769" height="203" border="1" align="center" cellpadding="0" cellspacing="0">
+        <?php 
 
-        <div class="botones_nav">
-            <a href="#" class="boton_home"></a>
-            <a href="#" class="boton_menu"></a>
-        </div>
-       <!-- <img src="imagenes/Logo/Logo_blanco_negro.svg" alt="logo"> -->
 
- 
+  
 
+//if(strlen($bxproducto)==0){
+//        $result=mysqli_query($connec,"select * from a_items where view01_it='S' AND grupolista_it like '%$xgl%' order by producto_it");
+//} else {
+//        $bxproducto1=trim($bxproducto);
+//        $result=mysqli_query($connec,"select * from a_items where producto_it like '%$bxproducto1%' order by producto_it");
+//}
 
-       <img src="iconos/logo_pmz.png" alt="logo" class="logo">
-    </nav>
-
-    <div class="barra_titulo">
-        <h2>Cerca a Ud. </h2>
-        <h3> Su: *Longitud=<?  echo ($longitud."  "); ?> *Latitud=<? echo ($latitud); ?> </h3>
-    </div>
-    
-    <div class="exterior">
-        
-        <div class="fondo_formulario">
- 
-          <div class="cabecera1">
-            <img src="imagenes/cabecera_formulario_iconos_alfa.png" alt="iconos" class="iconos">
-            </div>
-<?php 
+// $total=mysqli_num_rows($result);
 
 
 while ($tabla=mysqli_fetch_array($result)){
@@ -111,90 +168,60 @@ while ($tabla=mysqli_fetch_array($result)){
 	$longitud_aso=$tabla["longitud_aso"];
 	
 	
-	?>
-    <div class="lista"><a href="viewasociado.php?xcod=<?php  echo($cod_aso); ?>">
-    <table width="100%" border="0" cellpadding="0" cellspacing="0">
-    
-  <tr>
-    <td width="47" rowspan="2" valign="middle"><img src="img_asociados/<?php  echo($favicon_aso); ?>" width="45" height="45"></td>
-    <td width="226" class="busrazon"><?php  echo("  ".$rsocial_aso); ?></td>
-    <td width="64" rowspan="2" class="busdireccion"><?php  echo($direccion_aso); ?></td>
-  </tr>
-  <tr>
-    <td valign="middle" class="busproductos"><?php  echo(substr($productos_aso,0,45)); ?></td>
-    </tr>
-  <tr>
-    <td width="47">&nbsp;</td>
-    <td colspan="2" class="busraya">--------------------------------------------------------------------------------------------</td>
-    </tr>
-   
-    </table>
+	
+		
+		//if ($pv01_it<=$precom_it) { $color1="#FF0000";  } else {  $color1="#E4E4E4";  }
+		//if ($pv02_it<=$precom_it) { $color2="#FF0000";  } else {  $color2="#E4E4E4";  }
+		//if ($pv03_it<=$precom_it) { $color3="#FF0000";  } else {  $color3="#E4E4E4";  }
+        
+		//$lugar_al_it=$tabla["lugar_al_it"];	
 
-  </a></div>
- 
-<?php 
-       
-	} 
+		//$monelista_it=$tabla["monelista_it"];
+        //if ($monelista_it=="S") {
+          $simbolo_mone="S/  ";
+		//} else {
+        //    $simbolo_mone="US$ ";
+		//}
 
-mysqli_close($connec_sql);
-?>            
-            
+?>
+        
+        <tr bgcolor="#FFFFFF" class="tabla20">
+          <td width="767" height="175" valign="middle" bgcolor="#FFFFFF">
+            <table width="751" border="0" cellspacing="0" cellpadding="1">
+              <tr>
+                 <td width="47" rowspan="2" valign="middle"><img src="img_asociados/<?php  echo($favicon_aso); ?>" width="45" height="45"></td>
+                 
+                <td width="310" height="49" align="center" bgcolor="#FFFFFF"><span class="viewtexto"><?php echo($rsocial_aso) ?></span></td>
+              </tr>
+              <tr>
+                <td height="46" align="center" bgcolor=#FFFFFF ><span class="PRECIO1">
+                <?php echo($direccion_aso) ?>
+                </td>
+              </tr>
+              <tr> 
 
-
-          </div>   <!-- geolocalizacion-->     
-    <!-- FIN DE boton obligatorio -->              
+                <td height="59" align="center"><span class="once"><?php echo($msjpublico_it); ?></span>  
+                               
+              <!--  <span class="PRECIO2"> <?php // echo($simbolo_mone.money_format('%n',round($pv01_it*1.05/2,0))."      / ".$simbolo_mone.money_format('%n',round($pv01_it*1.15/3,0)))."<br>" ." 02/cuotas      / "." 03/cuotas " ?></span> -->
+                
+                </td>             
+              </tr>
+          </table></td>
+          
+          <!--- <td align="right" bgcolor=<? echo($color1) ?> ><?php echo($simbolo_mone.money_format('%n',(round($precom_it+($precom_it*$pje1_it/100))))) ?></td>-->        </tr>
+        <?php 
+	}
   
-</div>
-</div>
+?>
 
+      </table> 
+      <!-- FFFIIINNN  MUESTRA ITEMS -->
+      
+      </td>
+  </tr>
+  
+</table>
 
-
-
-    <footer>
-        <div class="footer_clientes footer_uno">
-            <article class="footer_article">
-                <h2 class="semi-titulos">CLIENTES</h2>
-                <p>Dencuentro.com es una página gratuita.
-                Las consultas por esta Web y los contactos 
-                con nuestros asociados son gratuitos.</p>
-            </article>
-        </div>
-
-        <div class="footer_asociados footer_uno">
-            <article class="footer_article">
-                <h2 class="semi-titulos">ASOCIADOS</h2>
-                <p>Brinda los siguientes servicios:
-                    * Servicio gratuito incluye:
-                    - El registro de datos, ubicación y productos que comercializa.
-                     
-                    * Servicio por convenio:
-                    - Diseño logos
-                    - Mapa Google.
-                </p>
-            </article>
-        </div>
-
-        <div class="footer_clientes footer_uno">
-            <article class="footer_article">
-                <h2 class="semi-titulos">PRODUCTOS</h2>
-                <p>Nuestra empresa, no se responsabiliza por productos o servicos en mal estado.</p>
-            </article>
-        </div>
-
-        <div class="footer_contactos footer_uno">
-            <article class="footer_article">
-                <h2 class="semi-titulos">CONTACTOS</h2>
-                <p>Cel. 959956000 <br>
-                    Diseño Web.
-                    Ca. Sena 105 cop. 58.
-                    J.L.B. y Rivero - Arequipa - Perú</p>
-            </article>
-        </div>
-
-        <div class="footer_logo">
-            <img src="imagenes/Logo/Logo_blanco_puro.svg" alt="">
-            <h3>Arequipa - 2020</h3>
-        </div>
-    </footer>
+<p>&nbsp;</p>
 </body>
 </html>
---
+++ siga_pormizona/buscar_pormizona_antiguo_full_css.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="fuentes.css">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="estilos-footer.css">
+    <link rel="icon" href="imagenes/dencuentro.ico" />
+    <style type="text/css">
+    a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+    </style>
+</head>
+<body>
+<?    
+include("connec_sql_new.php");
+mysqli_set_charset($connec,'utf8'); 
+date_default_timezone_set("America/Lima");
+setlocale(LC_ALL, "sp");
+
+$longitud = $_POST['longitude'];
+$latitud = $_POST['latitude'];
+
+//echo ("Longitud: ".$longitud."<br>");
+//echo ("Latitud : ".$latitud."<br>");
+if ($longitud<0) {
+     $longitud = substr($longitud,0,6);
+} else {
+     $longitud = substr($longitud,0,5);
+}
+if ($latitud<0) {
+     $latitud = substr($latitud,0,6);
+} else {
+     $latitud = substr($latitud,0,5);
+}
+
+//echo ("Longitud: ".$longitud."<br>");
+//echo ("Latitud : ".$latitud."<br>");
+
+$result=mysqli_query($connec,"select * from asociado_51 where (latitud_aso like '%$latitud%' AND longitud_aso like '%$longitud%') order by categoria_aso");
+
+$total=mysqli_num_rows($result);
+//echo "Total: ".$total."<br>";
+
+?>
+
+
+    <nav>
+
+        <div class="botones_nav">
+            <a href="#" class="boton_home"></a>
+            <a href="#" class="boton_menu"></a>
+        </div>
+       <!-- <img src="imagenes/Logo/Logo_blanco_negro.svg" alt="logo"> -->
+
+ 
+
+
+       <img src="iconos/logo_pmz.png" alt="logo" class="logo">
+    </nav>
+
+    <div class="barra_titulo">
+        <h2>Cerca a Ud. </h2>
+        <h3> Su: *Longitud=<?  echo ($longitud."  "); ?> *Latitud=<? echo ($latitud); ?> </h3>
+    </div>
+    
+    <div class="exterior">
+        
+        <div class="fondo_formulario">
+ 
+          <div class="cabecera1">
+            <img src="imagenes/cabecera_formulario_iconos_alfa.png" alt="iconos" class="iconos">
+            </div>
+<?php 
+
+
+while ($tabla=mysqli_fetch_array($result)){
+	$id=$tabla["id"];
+	$cod_aso=$tabla["cod_aso"];
+	$pais_aso=$tabla["pais_aso"];
+	$rsocial_aso=$tabla["rsocial_aso"];
+	$direccion_aso=$tabla["direccion_aso"];
+	$distrito_aso=$tabla["distrito_aso"];
+	$provincia_aso=$tabla["provincia_aso"];
+	$estado_aso=$tabla["estado_aso"];
+	$referencia_aso=$tabla["referencia_aso"];
+	$telf1_aso=$tabla["telf1_aso"];
+	$telf2_aso=$tabla["telf2_aso"];
+	$email_aso=$tabla["email_aso"];
+	$date_aso=$tabla["date_aso"];
+	$categoria_aso=$tabla["categoria_aso"];
+	$productos_aso=$tabla["productos_aso"];
+	$favicon_aso=$tabla["favicon_aso"];
+	if(strlen($favicon_aso)==0) {
+		$favicon_aso="f_pmz_bl.png";
+	}
+	$latitud_aso=$tabla["latitud_aso"];
+	$longitud_aso=$tabla["longitud_aso"];
+	
+	
+	?>
+    <div class="lista"><a href="viewasociado.php?xcod=<?php  echo($cod_aso); ?>">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0">
+    
+  <tr>
+    <td width="47" rowspan="2" valign="middle"><img src="img_asociados/<?php  echo($favicon_aso); ?>" width="45" height="45"></td>
+    <td width="226" class="busrazon"><?php  echo("  ".$rsocial_aso); ?></td>
+    <td width="64" rowspan="2" class="busdireccion"><?php  echo($direccion_aso); ?></td>
+  </tr>
+  <tr>
+    <td valign="middle" class="busproductos"><?php  echo(substr($productos_aso,0,45)); ?></td>
+    </tr>
+  <tr>
+    <td width="47">&nbsp;</td>
+    <td colspan="2" class="busraya">--------------------------------------------------------------------------------------------</td>
+    </tr>
+   
+    </table>
+
+  </a></div>
+ 
+<?php 
+       
+	} 
+
+mysqli_close($connec_sql);
+?>            
+            
+
+
+          </div>   <!-- geolocalizacion-->     
+    <!-- FIN DE boton obligatorio -->              
+  
+</div>
+</div>
+
+
+
+
+    <footer>
+        <div class="footer_clientes footer_uno">
+            <article class="footer_article">
+                <h2 class="semi-titulos">CLIENTES</h2>
+                <p>Dencuentro.com es una página gratuita.
+                Las consultas por esta Web y los contactos 
+                con nuestros asociados son gratuitos.</p>
+            </article>
+        </div>
+
+        <div class="footer_asociados footer_uno">
+            <article class="footer_article">
+                <h2 class="semi-titulos">ASOCIADOS</h2>
+                <p>Brinda los siguientes servicios:
+                    * Servicio gratuito incluye:
+                    - El registro de datos, ubicación y productos que comercializa.
+                     
+                    * Servicio por convenio:
+                    - Diseño logos
+                    - Mapa Google.
+                </p>
+            </article>
+        </div>
+
+        <div class="footer_clientes footer_uno">
+            <article class="footer_article">
+                <h2 class="semi-titulos">PRODUCTOS</h2>
+                <p>Nuestra empresa, no se responsabiliza por productos o servicos en mal estado.</p>
+            </article>
+        </div>
+
+        <div class="footer_contactos footer_uno">
+            <article class="footer_article">
+                <h2 class="semi-titulos">CONTACTOS</h2>
+                <p>Cel. 959956000 <br>
+                    Diseño Web.
+                    Ca. Sena 105 cop. 58.
+                    J.L.B. y Rivero - Arequipa - Perú</p>
+            </article>
+        </div>
+
+        <div class="footer_logo">
+            <img src="imagenes/Logo/Logo_blanco_puro.svg" alt="">
+            <h3>Arequipa - 2020</h3>
+        </div>
+    </footer>
+</body>
+</html>
