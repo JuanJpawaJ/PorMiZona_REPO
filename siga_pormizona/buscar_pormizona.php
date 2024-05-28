3ff2ed5bbdfbdcbe94548ea01336ee2cc66ad391
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-05-28 17:44:14 -0500

    tavle 1

--- siga_pormizona/buscar_pormizona.php
+++ siga_pormizona/buscar_pormizona.php
@@ -128,7 +128,7 @@ $total=mysqli_num_rows($result);
   <tr class="tit_menu_sup">
     <td height="118" valign="top" bgcolor="#FFFFCC">
       <!-- INICIO DE MUESTRA ITEMS -->
-      <table width="769" height="99" border="1" align="center" cellpadding="0" cellspacing="0">
+      <table width="769" height="99" align="center" cellpadding="0" cellspacing="0"  style="border: 1px solid red;">
         <?php 
 
 
