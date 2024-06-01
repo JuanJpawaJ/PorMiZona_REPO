0000de09b6b044672b136e502ac18ff31e578b8c
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-06-01 00:26:30 -0500

    prueba

--- siga_pormizona/areg_mod_asoc.php
+++ siga_pormizona/areg_mod_asoc.php
@@ -22,10 +22,10 @@ if      ($form=="00") {
    $latitud_aso=$_POST['xlatitud'];
    $longitud_aso=$_POST['xlongitud'];	
    
-  // echo ("form :").$form;
-  // echo ("cod :").$cod;
-  // echo ("lati :").$latitud_aso;
-  // echo ("long :").$longitud_aso;
+   echo ("form :").$form;
+   echo ("cod :").$cod;
+   echo ("lati :").$latitud_aso;
+   echo ("long :").$longitud_aso;
    
    $sql="UPDATE asociado_51 SET latitud_aso='$latitud_aso',longitud_aso='$longitud_aso' WHERE cod_aso=$cod";
    $result=mysqli_query($connec,$sql);
