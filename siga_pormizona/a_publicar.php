76677521227bc27bba17a8cc0ef22de69304e2f6
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-09-10 18:04:59 -0500

    nooo

---
+++ siga_pormizona/a_publicar.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <link rel="stylesheet" href="css/normalizar.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilos_footer.css">
+    <link rel="stylesheet" href="css/estilos_navegador.css">
+  <style>
+      .button {
+          display: inline-block;
+          transition: transform 0.3s ease; /* Suaviza la transición */
+      }
+
+      .button:hover {
+          transform: scale(1.1); /* Aumenta el tamaño de la imagen al pasar el mouse */
+      }
+  </style>
+
+<title>PUBLICAR AVISOS</title> 
+
+</head>
+<body>
+    <header>
+        <div>
+            <img src="imgs/pmz_01_rgb.jpg" alt="" class="logo_por_mi_zona">
+            <img src="imgs/c_edi_01_rgb.jpg" alt="" class="central_de_edicion">
+            
+            <div class="contenedor_navegador">
+                <button class="nav" id="boton_hamburguesa" onclick="desplegarBotonHamburguesa()">
+                    <span class="material-symbols-outlined">
+                        menu
+                    </span>
+                </button>
+    
+            </div>
+
+        </div>
+        <div>
+            <h2>ENCUENTRE LO QUE NECESITE CERCA DE USTED</h2>
+            <h2>PUBLICIDAD LOCALIZADA Y EVENTOS</h2>
+        </div>
+       <div class="contenedor_botones"> 
+
+            <a href="siga_pormizona/buscar_pormizona.php" class="button"><img src="imgs/b_tiendas.png" alt="Botón 1" ></a>
+            <a href="" class="button"><img src="imgs/b_eventos.png" alt="Botón 2" ></a>
+            <a href="siga_pormizona/a_cat_impreso.php" class="button"><img src="imgs/b_catalogo.png" alt="Botón 3" ></a>
+            <a href="" class="button button_text_img">
+            	<p>este es un texto</p>
+            	<img src="imgs/b_publicar.png" alt="Botón 4" >
+            </a>
+<!---
+
+    <a href="https://example.com" class="button"><img src="button1.png" alt="Botón 1">
+    </a>
+    <a href="https://example.com" class="button">
+        <img src="button2.png" alt="Botón 2">
+    </a>
+
+
+--->
+
+
+
+
+        </div>
+    </header>
+    <div class="division"></div>
+    <div class="empresas_asociadas">
+        <h1>EMPRESAS ASOCIADAS</h1>
+        <div class="contenedor_empresas">
+            <div class="empresa">
+                <div>
+                   HOLA 1 
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    HOLA 2
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+            
+  
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+  
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+
+            <div class="empresa">
+                <div>
+                    
+                </div>
+            </div>
+    </div>
+    </div>
+    <div class="division"></div>
+
+    <div class="footer">
+        <div>
+            <h4>Tienda exhibición:</h4>
+            <p>Av. Porongoche 323 (1/2 cuadra del Mall Aventura Plaza - Porongoche)</p>
+    
+            <h4>Almacen:</h4>
+            <p>Calle Sena 105 - Coop 58 (1/2 cuadra del Mall Aventura Porongoche)</p>
+        </div>
+    
+        <div>
+            <h4>Teléfonos:</h4>
+            <ul>
+                <li>959956000</li>
+                <li>959956060</li>
+            </ul>
+    
+            <h4>Correo electrónico:</h4>
+            <ul>
+                <li>jpawajsac@gmail.com</li>
+                <li>jpawajsac@gmail.com</li>
+            </ul>
+        </div>
+        <div>
+            <ul class="lista_de_redes_sociales">
+                <li>
+                    <a href="#">
+                        <img src="imgs/imagenes_index/logo_facebook_blanco.png" alt="">
+                        <p>instagram/jpawaj</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="imgs/imagenes_index/logo_facebook_blanco.png" alt="">
+                        <p>instagram/jpawaj</p>
+                    </a>
+                </li>
+    
+                <li>
+                    <a href="#">
+                        <img src="imgs/imagenes_index/logo_instagram_blanco.png" alt="">
+                        <p>instagram/jpawaj</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src="imgs/imagenes_index/logo_whatsapp_blanco.png" alt="">
+                        <p>instagram/jpawaj</p>
+                    </a>
+                </li>
+    
+                <li>
+                    <a href="#">
+                        <img src="imgs/imagenes_index/logo_whatsapp_blanco.png" alt="">
+                        <p>instagram/jpawaj</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    
+        <div class="contenedor_final_footer">
+            <img src="/imgs/imagenes_index/logo_JPAWAJ_blanco.png" alt="">
+            <p>Arequipa - Peru - 2024</p>
+        </div>
+    </div>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
