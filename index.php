dd808cf83d357bce0d9b000f52c238ffcf29edf5
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-09-13 22:57:55 -0500

    ya pues

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="css/estilos_footer.css">
     <link rel="stylesheet" href="css/estilos_navegador.css">
-    <link rel="stylesheet" href="estilos-footer.css">
+    <link rel="stylesheet" href="css/estilos-footer.css">
 
   <style>
       .button {
