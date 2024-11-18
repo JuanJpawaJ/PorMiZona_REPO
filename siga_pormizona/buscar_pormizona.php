79bfc91702c528665a99ef7dc8570eb2910b7258
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-11-18 13:57:35 -0500

    ya pues

--- siga_pormizona/buscar_pormizona.php
+++ siga_pormizona/buscar_pormizona.php
@@ -197,14 +197,14 @@ echo ("VOYYYYYYYY AAAAA IF xfi");
 if($xfi=="T") {
   $titulo="LISTA TODO con filtro";
   $result=mysqli_query($connec,"select * from asociado_51 where (productos_aso like '%$bxproducto%' OR rsocial_aso like '%$bxproducto%' OR gironeg_aso like '%$bxproducto%) order by categoria_aso");
-
+ //$result=mysqli_query($connec,"select * from asociado_51 where (productos_aso like '%$bxproducto%') order by categoria_aso");
 
 } else { // "Z" latitud y longitud
   echo ("ESTOY AQUIIIIIIIIII ZZZZZZZZZZZ");
 
   $titulo="LISTA POR MI ZONA con filtro";
-  $result=mysqli_query($connec,"select * from asociado_51 where (latitud_aso like '%$bxlatitud%' AND longitud_aso like '%$bxlongitud%') AND (productos_aso like '%$bxproducto%' OR rsocial_aso like '%$bxproducto%' OR gironeg_aso like '%$bxproducto%) order by categoria_aso");
-
+ // $result=mysqli_query($connec,"select * from asociado_51 where (latitud_aso like '%$bxlatitud%' AND longitud_aso like '%$bxlongitud%') AND (productos_aso like '%$bxproducto%' OR rsocial_aso like '%$bxproducto%' OR gironeg_aso like '%$bxproducto%) order by categoria_aso");
+ $result=mysqli_query($connec,"select * from asociado_51 where (latitud_aso like '%$bxlatitud%' AND longitud_aso like '%$bxlongitud%' AND (productos_aso like '%$bxproducto%' OR rsocial_aso like '%$bxproducto%' OR gironeg_aso like '%$bxproducto%)) order by categoria_aso");
 }
 
 $total=mysqli_num_rows($result);
