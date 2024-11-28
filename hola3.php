ce45456e92ec1791a5de10e8ef469655988acef6
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-11-28 15:11:49 -0500

    ffffyu7

--- hola3.php
+++ hola3.php
@@ -98,6 +98,54 @@ setlocale(LC_ALL, "sp");
         </div>
     </div>
 
+    <div class="empresas_asociadas">
+        <h1>EMPRESAS ASOCIADAS</h1>
+        <div class="contenedor_empresas">
+           <?   
+           include("connec_sql_new.php");
+           mysqli_set_charset($connec,'utf8'); 
+           date_default_timezone_set("America/Lima");
+           setlocale(LC_ALL, "sp");
+   
+           $titulo="LISTA POR MI ZONA con filtro";
+           $result=mysqli_query($connec,"select * from asociado_51 where view01_aso='S' ");
+           $total=mysqli_num_rows($result);        
+           while ($tabla=mysqli_fetch_array($result)){
+	         $id=$tabla["id"];
+	         $cod_aso=$tabla["cod_aso"];
+	         $pais_aso=$tabla["pais_aso"];
+	         $rsocial_aso=$tabla["rsocial_aso"];
+	         $direccion_aso=$tabla["direccion_aso"];
+	         $distrito_aso=$tabla["distrito_aso"];
+	         $provincia_aso=$tabla["provincia_aso"];
+	         $estado_aso=$tabla["estado_aso"];
+	         $referencia_aso=$tabla["referencia_aso"];
+	         $gironeg_aso=$tabla["gironeg_aso"];
+	         $telf1_aso=$tabla["telf1_aso"];
+	         $telf2_aso=$tabla["telf2_aso"];
+	         $email_aso=$tabla["email_aso"];
+	         $date_aso=$tabla["date_aso"];
+	         $categoria_aso=$tabla["categoria_aso"];
+	         $productos_aso=$tabla["productos_aso"];
+	         $favicon_aso=$tabla["favicon_aso"];
+	         $logo_aso=$tabla["logo_aso"];
+	         $link01_aso=$tabla["link01_aso"];
+	         if(strlen($favicon_aso)==0) {
+	    	    $favicon_aso="f_pmz_bl.png";
+	         }
+	         $latitud_aso=$tabla["latitud_aso"];
+	         $longitud_aso=$tabla["longitud_aso"];
+             ?>       
+             <div class="empresa">
+                <a href=" <? echo($link01_aso); ?> "> <img src="siga_pormizona/img_asociados/<? echo($logo_aso); ?>"  ></a>
+            <?  echo($gironeg_aso); ?>                
+            </div>
+      <? } ?>
+    </div>
+ </div>
+
+
+   <!---
     <div class="contenedor_marcas">
         <h3>NUESTROS PRINCIPALES CLIENTES</h3>
         <div class="contenedor_slider">
@@ -136,7 +184,7 @@ setlocale(LC_ALL, "sp");
             </div>
         </div>
     </div>
-
+-->
 
     
 
