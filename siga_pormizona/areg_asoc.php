c2399ea88dcac8692257de5be65828f2e7d6f0cf
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2024-12-15 12:48:15 -0500

    aquiii

--- siga_pormizona/areg_asoc.php
+++ siga_pormizona/areg_asoc.php
@@ -158,6 +158,7 @@ mysqli_close($connec);
 <? } else { ?>
     <!--- <th scope="col" class="semi-titulosform"><div align="center"><a href="formingre2.php?xcod=<?php  //echo($cod_aso); ?>">CONTINUAR CON EL PASO 3</a></div></th> -->
    <? 
+   echo "estoy aqui  y voy al header...";
     // Redirección automática a la siguiente página
      header("Location: formingre2.php?xcod=$cod_aso");
 	 exit(); // Asegúrate de llamar a exit después de la redirección
