7bb0dec86b6a4337f0a0aac5212b37252b033ca0
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-07 19:16:52 -0500

    widgggyhhh

---
+++ widgets/carta_producto.php
@@ -0,0 +1,35 @@
+<!-- <link rel="stylesheet" href="../assets/css/estilos_carta_producto.css"> -->
+<?php
+    
+    if ($view03 == 'S') {
+        $clase_oferta = 'carta_producto_oferta';
+        $texto_oferta = "<p>OFERTA</p>";
+        $precio_antiguo = "<p class='precio_antiguo'>{$precio}</p>";
+        $precio = $precio_oferta;
+        echo "<script>console.log('SI');</script>";
+        
+    } else {
+        $clase_oferta = '';
+        $texto_oferta = "";
+        $precio_antiguo = "";
+        echo "<script>console.log('NO');</script>";
+    }
+    
+?>
+
+<div class="carta_producto <?php echo $clase_oferta;?>">
+    <div class="contenedor_imagen"><img src="<?php echo $imagen;?>" /></div>
+    <h3>
+        <?php echo $nombre_producto;?>
+    </h3>
+    <div class="precios">
+        <?php echo $precio_antiguo;?>
+        <p class="precio_oferta">
+            <?php echo $precio;?>
+        </p>
+    </div>
+    <a href="<?php echo $enlace;?>">
+        Ver producto
+    </a>
+    <?php echo $texto_oferta;?>
+</div>
---
+++ widgets/navegador.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="<?php echo $agregado_en_cab?>assets/css/estilos_navegador.css">
+
+<div class="navegador">
+        <img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_rojo_blanco.png" alt="" class="logo_principal">
+
+        <button class="boton_nav" id="boton_hamburguesa" onclick="desplegarBotonHamburguesa()">
+            <span class="material-symbols-outlined">
+                menu
+            </span>
+        </button>
+
+        <div class="cont_lista_elementos" id="cont_elementos_navegador">
+            <ul>
+                <li><a href="<?php echo $agregado_en_cab?>#jpawaj"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_blanco.png" alt=""></a></li>
+                <li><a href="<?php echo $agregado_en_cab?>#syscomputer"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_syscomputer_blanco.png" alt=""></a></li>
+                <li><a href="http://mujerbonita.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_mujer_bonita_blanco.png"alt=""></a></li>
+                <li><a href="https://www.pormizona.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_pormizona_blanco.png" alt=""></a></li>
+                <li><a href="<?php echo $agregado_en_cab?>#interclass"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_interclass_blanco.png" alt=""></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+<div class="navegador_espaciado"></div>
+
+
+<script src="<?php echo $agregado_en_cab?>assets/js/navegador.js"></script>
