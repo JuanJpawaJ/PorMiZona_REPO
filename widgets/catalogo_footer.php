fd786dc0930f5ced31720ccbb7925fbc72e0d855
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-08 23:49:34 -0500

    yanose765

--- widgets/catalogo_footer.php
+++ widgets/catalogo_footer.php
@@ -60,7 +60,7 @@
     </div>
 
     <div class="contenedor_final_footer">
-        <img src="<? echo $agregado_en; ?>.siga_catalogo/img_catacli/<? echo $logo_aso; ?>" alt="">
+        <img src="<? echo $agregado_en; ?>siga_catalogo/img_catacli/<? echo $logo_aso; ?>" alt="">
         <p><? echo $departamentotxt_aso; ?> - Peru - 2025</p>
     </div>
     
