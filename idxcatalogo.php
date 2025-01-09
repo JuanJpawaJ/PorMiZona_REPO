f0572efa34a9f29244c911fceffeb87112082593
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-08 22:12:35 -0500

    PRUEBA DE FOOTER

--- idxcatalogo.php
+++ idxcatalogo.php
@@ -243,9 +243,83 @@ $obsinterno_aso=$tabla["obsinterno_aso"];
 
 
     <?php 
-    $agregado_en = "";
-    include 'widgets/catalogo_footer.php' 
+   // $agregado_en = "";
+   // include 'widgets/catalogo_footer.php' 
     ?>
+    
+    <!-- inicio de footer -->
+<link rel="stylesheet" href="<?php echo $agregado_en?>assets/css/estilos_footer.css">
+
+<div class="footer">
+    <div>
+        <h4>Tienda exhibición:</h4>
+        <p>Av. Porongoche 323 (1/2 cuadra del Mall Aventura Plaza - Porongoche)</p>
+
+        <h4>Almacen:</h4>
+        <p>Calle Sena 105 - Coop 58 (1/2 cuadra del Mall Aventura Porongoche)</p>
+    </div>
+
+    <div>
+        <h4>Teléfonos:</h4>
+        <ul>
+            <li>959956000</li>
+            <li>959956060</li>
+        </ul>
+
+        <h4>Correo electrónico:</h4>
+        <ul>
+            <li>jpawasac@gmail.com</li>
+            <li>jpawasac@gmail.com</li>
+        </ul>
+    </div>
+    <div>
+        <ul class="lista_de_redes_sociales">
+            <li>
+                <a href="#">
+                    <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_facebook_blanco.png" alt="">
+                    <p>instagram/jpawaj</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_facebook_blanco.png" alt="">
+                    <p>instagram/jpawaj</p>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_instagram_blanco.png" alt="">
+                    <p>instagram/jpawaj</p>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_whatsapp_blanco.png" alt="">
+                    <p>instagram/jpawaj</p>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_whatsapp_blanco.png" alt="">
+                    <p>instagram/jpawaj</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="contenedor_final_footer">
+        <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_JPAWAJ_blanco.png" alt="">
+        <p>Arequipa - Peru - 2024</p>
+    </div>
+    
+</div>
+
+<div class="m_p">
+    Desarrollador: Juan Diego Valdivia Mendoza
+</div>    
+    <!----- FINAL DE FOOTER  ---->
 
     <script src="assets/js/slider_automatico.js"></script>
     <script src="assets/js/slider_marcas.js"></script>
