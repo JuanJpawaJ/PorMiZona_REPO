6703049967f372238c0eb686893ab21877809c5a
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-16 22:40:09 -0500

    otravez33

--- siga_catalogo/catalogo_list_items_admin.php
+++ siga_catalogo/catalogo_list_items_admin.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>iTEMS Administrador</title>
+<title>Administrador Productos</title>
 <style type="text/css">
 .TITULO_NARANJA {
 	color: #FC0;
