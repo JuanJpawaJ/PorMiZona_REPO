b59ead7a59de66ad10b5b0e076fad764c0888542
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-21 17:50:38 -0500

    varios logos y otros

--- widgets/catalogo_navegador.php
+++ widgets/catalogo_navegador.php
@@ -2,17 +2,15 @@
 
 <div class="navegador">
 
-<?
-if (strlen(trim($logo_aso))==0) { 
+<?      if (strlen(trim($logo_aso))==0) { 
 
          echo "<span class='razonsocial_blanco'>$rsocial_aso</span>"; ?>
 
-
-<? } else { ?>
+   <? } else { ?>
 
         <img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_rojo_blanco.png" alt="" class="logo_principal">
 
-<? } ?>
+   <? } ?>
         <button class="boton_nav" id="boton_hamburguesa" onclick="desplegarBotonHamburguesa()">
             <span class="material-symbols-outlined">
                 menu
@@ -20,16 +18,22 @@ if (strlen(trim($logo_aso))==0) {
         </button>
 
         <div class="cont_lista_elementos" id="cont_elementos_navegador">
-            <ul>
-                <li><a href="<?php echo $agregado_en_cab?>#jpawaj"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_blanco.png" alt=""></a></li>
-                <li><a href="<?php echo $agregado_en_cab?>#syscomputer"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_syscomputer_blanco.png" alt=""></a></li>
-                <li><a href="http://mujerbonita.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_mujer_bonita_blanco.png"alt=""></a></li>
-                <li><a href="https://www.pormizona.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_pormizona_blanco.png" alt=""></a></li>
-                <li><a href="<?php echo $agregado_en_cab?>#interclass"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_interclass_blanco.png" alt=""></a>
-                </li>
-            </ul>
+          <ul>
+      <!-- <li><a href="<?php echo $agregado_en_cab?>#jpawaj"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_blanco.png" alt=""></a></li>
+           <li><a href="<?php echo $agregado_en_cab?>#syscomputer"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_syscomputer_blanco.png" alt=""></a></li>
+           <li><a href="http://mujerbonita.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_mujer_bonita_blanco.png"alt=""></a></li>
+           <li><a href="https://www.pormizona.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_pormizona_blanco.png" alt=""></a></li>
+           <li><a href="<?php echo $agregado_en_cab?>#interclass"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_interclass_blanco.png" alt=""></a></li>
+       -->   
+           <li><a href="<?php echo $agregado_en_cab?>#syscomputer"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/bot_incio_blanco.png" alt=""></a></li>
+           <li><a href="http://mujerbonita.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/bot_productos_blanco.png"alt=""></a></li>
+           <li><a href="http://mujerbonita.com.pe"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/bot_ubicacion_blanco.png"alt=""></a></li>
+
+       
+       
+          </ul>
         </div>
-    </div>
+ </div>
 
 <div class="navegador_espaciado"></div>
 
