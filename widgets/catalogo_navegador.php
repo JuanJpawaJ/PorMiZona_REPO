183850c79e42ba299971b809420daad10216e056
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-21 22:53:32 -0500

    uukkkooo

--- widgets/catalogo_navegador.php
+++ widgets/catalogo_navegador.php
@@ -8,8 +8,7 @@
 
    <? } else { ?>
 
-        <img src="img_asociados/<? echo $logo_aso; ?>" alt="" class="logo_principal">
-        <img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_rojo_blanco.png" alt="" class="logo_principal">
+        <img src="../img_asociados/<? echo $logo_aso; ?>" alt="" class="logo_principal">
 
    <? } ?>
         <button class="boton_nav" id="boton_hamburguesa" onclick="desplegarBotonHamburguesa()">
