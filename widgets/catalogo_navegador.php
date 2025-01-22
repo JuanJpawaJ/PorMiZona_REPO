827f6a200e06d4e5a505ff457da42151b73e1367
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-21 22:16:18 -0500

    logouuyw

--- widgets/catalogo_navegador.php
+++ widgets/catalogo_navegador.php
@@ -8,7 +8,7 @@
 
    <? } else { ?>
 
-        <img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_rojo_blanco.png" alt="" class="logo_principal">
+        <img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/<? echo $logo_aso; ?>" alt="" class="logo_principal">
 
    <? } ?>
         <button class="boton_nav" id="boton_hamburguesa" onclick="desplegarBotonHamburguesa()">
