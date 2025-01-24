55070b6e8d0ecef96ede51efb595803a1f578ee9
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-23 19:03:03 -0500

    haber a77y

--- siga_catalogo/img_catacli/n_subir_xfile.php
+++ siga_catalogo/img_catacli/n_subir_xfile.php
@@ -3,10 +3,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>:: Importar documentos con CODIGO** ::</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="estilos_cat/normalize.css">
-    <link rel="stylesheet" href="estilos_cat/fuentes.css">
-    <link rel="stylesheet" href="estilos_cat/estilos.css">
-    <link rel="stylesheet" href="estilos_cat/estilos-footer.css">
+   <link rel="stylesheet" href="../estilos_cat/normalize.css">
+    <link rel="stylesheet" href="../estilos_cat/fuentes.css">
+    <link rel="stylesheet" href="../estilos_cat/estilos.css">
+    <link rel="stylesheet" href="../estilos_cat/estilos-footer.css">
    <!-- <link rel="icon" href="imagenes/dencuentro.ico" />   -->
 </head>
 
