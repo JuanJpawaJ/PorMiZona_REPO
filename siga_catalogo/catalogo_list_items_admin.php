cc45ccf5c91bce786abc9844dd375f8c72de1a6e
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-01-29 19:31:54 -0500

    ññopl

--- siga_catalogo/catalogo_list_items_admin.php
+++ siga_catalogo/catalogo_list_items_admin.php
@@ -297,7 +297,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['whatsapp_number'])) {
 <button id="showFormBtn" style="background-color: #25D366; color: white; border: none; padding: 10px 20px; margin: 10px; cursor: pointer; border-radius: 5px; font-size: 16px;">ENVIAR catálogo por WhatsApp</button>  -->
 
   <div class="form-container">
-    <form action="" method="POST">
+    <form action="catalogo_list_items.php" method="POST">
         <input type="text" name="whatsapp_number" class="form-input" placeholder="Número de WhatsApp" required>
         <button type="submit" class="btn">Enviar</button>
     </form>
