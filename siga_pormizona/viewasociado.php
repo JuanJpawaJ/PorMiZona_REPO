39b9e27b8c46dced4300d56a8b8e74087f274ac9
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-02-04 20:22:52 -0500

    ELIMIDDGG

--- siga_pormizona/viewasociado.php
+++ siga_pormizona/viewasociado.php
@@ -197,7 +197,7 @@ $obsinterno_aso=$tabla["obsinterno_aso"];
 
 
 
-    <table width="823" height="1175" border="0" align="center" cellpadding="0" cellspacing="0">
+    <table width="823" height="1158" border="0" align="center" cellpadding="0" cellspacing="0">
       <tr>
         <td width="815" height="142" colspan="2" align="center" valign="middle" background="iconos/f_cabview.jpg" >
 		<table width="772" border="0" cellspacing="0" cellpadding="0">
@@ -229,19 +229,7 @@ $obsinterno_aso=$tabla["obsinterno_aso"];
         <td height="305" colspan="2" align="center" valign="middle" bgcolor="#EEF6F8"><img src="<?php echo "img_asociados/".$img1_aso ?>" width="<? echo $vw_ancho; ?>"  height="%"></td>
       </tr>
       <tr>
-        <td height="53" colspan="2" align="center" valign="middle" bgcolor="#EEF6F8" class="productos_aso"><? echo $productos_aso."<br>"; ?></td>
-      </tr>
-      <tr>
-        <td height="68" colspan="2" align="center" valign="middle" bgcolor="#EEF6F8"><table width="759" border="0" align="center" cellpadding="0" cellspacing="0">
-
-  <tr>
-    <td width="489" height="28" class="txt_view_dir_otros" ><? echo $categoria_aso.": ".$categoriatxt_aso."<br>"; ?></td>
-    <td width="142" height="28" class="txt_view_dir_otros" ><? echo $email_aso; ?></td>
-  </tr>
-  <tr>
-    <td height="32" colspan="3" class="txt_view_dir_otros"><? echo $msjpublico_aso; ?></td>
-    </tr>
-</table></td>
+        <td height="42" colspan="2" align="center" valign="middle" bgcolor="#EEF6F8" class="productos_aso"><? echo $productos_aso."<br>"; ?></td>
       </tr>
       <tr>
         <td height="11" colspan="2" align="center" bgcolor="#000000" class="txt_view_pie"><table width="773" border="0" cellspacing="0" cellpadding="0">
