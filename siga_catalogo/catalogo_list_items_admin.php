3bab3e616a7a104f29810deb43ebdfb20cc611b1
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-02-07 23:41:24 -0500

    probare

--- siga_catalogo/catalogo_list_items_admin.php
+++ siga_catalogo/catalogo_list_items_admin.php
@@ -175,7 +175,7 @@ if ($xdelreg=="SIDELREG") {
 if ($whatsapp=="SIWHATSAPP") {
     $whatsapp_number = $_POST['whatsapp_number'];
     $link = "https://www.pormizona.com.pe/idxcatalogo.php?xcod=" . $cod_aso . "&xrsocial=" . $rsocial_aso;
-    $whatsapp_url = "https://api.whatsapp.com/send?phone=" . $whatsapp_number . "&text=" . "CATALOGO: ".$rsocial_aso." ".$link. "Para ver mi enlace *ACEPTAR COMO CONTACTO*";
+    $whatsapp_url = "https://api.whatsapp.com/send?phone=" . $whatsapp_number . "&text=" ." *ACEPTAR COMO CONTACTO* ". " CATALOGO: ".$rsocial_aso." ".$link ;
 
     //$whatsapp_url = "https://wa.me/$whatsapp_number?text=Hola%20deseo%20información%20de:%20$link target='_blank'"; 
 
