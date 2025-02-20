a97b4358ddfa1dc7ef60b2b0a02654cd4688964d
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-02-19 19:47:31 -0500

    ahira si subir

--- siga_catalogo/catalogo_list_items_admin.php
+++ siga_catalogo/catalogo_list_items_admin.php
@@ -2,7 +2,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<title>Administrador Productos1</title>
+<title>Administrador Productos</title>
 
 <link rel="icon" href="../assets/img/imagenes_index/icono_pestana/icono_catalogo.png" type="image/png">
 
--- siga_pormizona/a_list_asociados_admin.php
+++ siga_pormizona/a_list_asociados_admin.php
@@ -1,7 +1,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="icon" href="../assets/img/imagenes_index/icono_pestana/icono_pormizona.png" type="image/png">
 <title>iTEMS Administrador</title>
+
+
 <style type="text/css">
 .TITULO_NARANJA {
 	color: #FC0;
