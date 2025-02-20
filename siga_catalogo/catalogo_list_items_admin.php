55b1ef37c8472bdbbaf5fd940cad9b54e4989cd9
JuanJpawaJ <167272004+JuanJpawaJ@users.noreply.github.com>
2025-02-19 19:04:14 -0500

    iconaaset

--- siga_catalogo/catalogo_list_items_admin.php
+++ siga_catalogo/catalogo_list_items_admin.php
@@ -1,7 +1,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <title>Administrador Productos1</title>
+
+<link rel="icon" href="../assets/img/imagenes_index/icono_pestana/icono_catalogo.png" type="image/png">
+
 <style type="text/css">
 .TITULO_NARANJA {
 	color: #FC0;
